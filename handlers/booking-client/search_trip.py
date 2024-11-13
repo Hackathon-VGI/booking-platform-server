7c5c486bd241ad8164f2228d725f30850289a5e3
Abdul Bari <abdulbari3635@gmail.com>
2024-11-13 19:28:12 +0100

    updated search_trips api

--- handlers/booking-client/search_trip.py
+++ handlers/booking-client/search_trip.py
@@ -3,7 +3,6 @@ from pymongo import MongoClient
 from flask_cors import CORS
 import os
 import certifi
-from datetime import datetime
 import gtfs_to_mongo as utils
 
 app = Flask(__name__)
@@ -11,16 +10,13 @@ CORS(app)
 
 
 # Initialize MongoDB client
-# mongo_uri = os.getenv("MONGO_URI")
-MONGO_URI="mongodb+srv://zainanwer24:osyP2q9A6L83y4Ku@booking-platform.w3axp.mongodb.net/"
-mongo_uri = MONGO_URI
-
+mongo_uri = os.getenv("MONGO_URI")
 client = MongoClient(mongo_uri, tlsCAFile=certifi.where())
 db = client["Booking-App-VGI"]
 stop_times = db["stop_times"]
 stops = db["stops"]
 
-# util function
+# util functions
 def get_bus_number(trip_id):
     trip_id = trip_id.split(":")
     return trip_id[0]
@@ -34,6 +30,13 @@ def time_in_seconds(time):
     time_seconds = (hours*3600) + (mins*60) + (seconds)
     return time_seconds
 
+def adjust_time(time_str):
+    parts = time_str.split(":")
+    hours = int(parts[0])
+    if hours >= 24:
+        hours -= 24
+        parts[0] = f"{hours:02d}"
+    return ":".join(parts)
 
 # Define the route to search trips
 @app.route("/api/search-trip", methods=["GET"])
@@ -43,10 +46,9 @@ def search_trips():
     departure_stop = trip_details.get('departure_stop')  
     arrival_stop = trip_details.get('arrival_stop') 
 
-    ### still need to implement seat logic     
-    # required_seats = trip_details.get('required_seats') 
+    required_seats = trip_details.get('required_seats') 
+    
     departure_time = trip_details.get('departure_time') 
-
     departure_time_seconds = time_in_seconds(departure_time)
     
     all_trips = utils.find_trips(departure_stop, arrival_stop)
@@ -55,15 +57,26 @@ def search_trips():
 
     if all_trips:
         for trip in all_trips:
+            trip_id = trip["trip_id"]
+            available_seats = stop_times.find_one({"trip_id" : trip_id}).get("max_seats")
+
             # Get the departure time of the trip from the database
             trip_departure_time = trip['departure_time']
             trip_departure_time_seconds = time_in_seconds(trip_departure_time)
 
-            # Compare times
-            if departure_time_seconds <= trip_departure_time_seconds:
+            trip["departure_time"] = adjust_time(trip_departure_time)
+            trip["arrival_time"] = adjust_time(trip["arrival_time"])
+
+            # Compare times and seats
+            if departure_time_seconds <= trip_departure_time_seconds and available_seats >= required_seats:
                 valid_routes.append(trip)
 
-    return jsonify(valid_routes)
+    return jsonify({
+        "valid_routes" : valid_routes,
+        "departure_stop" : departure_stop,
+        "arrival_stop" : arrival_stop,
+        "departure_time" : departure_time
+                     })
 
 
 if __name__ == "__main__":
