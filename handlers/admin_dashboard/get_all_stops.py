649c4736323b519a955a126c7af3c3cd6c3cde7b
Syed Zain Anwer <zainanwer24@gmail.com>
2024-11-15 09:36:03 +0100

    commented out print statements

--- handlers/admin_dashboard/get_all_stops.py
+++ handlers/admin_dashboard/get_all_stops.py
@@ -8,5 +8,5 @@ stops_collection = mongo_db['stops']
 def get_all_stops():
     # Query the stops collection and retrieve all stop names
     stop_names = stops_collection.distinct("stop_name")  # Use 'distinct' to get unique stop names
-    print(stop_names)
+    # print(stop_names)
     return jsonify({"message":"success", "stop_names": stop_names})
--- handlers/admin_dashboard/toggle_stop_status.py
+++ handlers/admin_dashboard/toggle_stop_status.py
@@ -8,21 +8,26 @@ stop_times = mongo_db["stop_times"]
 user_trip_details = mongo_db["user_trip_details"]
 stops = mongo_db["stops"]
 
+
 def toggle_stop_status():
+  '''
+    Admin would be block stop
+    Stop with same names would be updated together
+  '''
   data = request.json
   stop_name = data.get("stop_name")
+  stop_blocked = data.get("stop_blocked")
 
-  current_stop_status = stops.find_one({"stop_name" : stop_name}).get("stop_blocked")
+  if not stop_name:
+    return jsonify({"message": "stop_name is required"}), 400
 
-  if not current_stop_status:
-    stops.update_one(
-    {"stop_name": stop_name},  # Find the document where stop_name matches
-    {"$set": {"stop_blocked": True}}  # Set stop_blocked to True
-)
-  else:
-    stops.update_one(
-    {"stop_name": stop_name},  # Find the document where stop_name matches
-    {"$set": {"stop_blocked": False}}  # Set stop_blocked to True
-)
-  return jsonify({"message":"status changed successfully"})
+  try:
+    # Find all stops with the same stop_name
+      stops.update_many(
+        {"stop_name": stop_name},  # Filter by stop_name
+          {"$set": {"stop_blocked": stop_blocked}}  # Update stop_blocked to the new status
+      )
+      return jsonify({"message": "Stop status updated successfully"}), 200
+  except Exception as e:
+    return jsonify({"message": str(e)}), 500
 
